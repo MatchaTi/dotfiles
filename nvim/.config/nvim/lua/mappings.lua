92363dd15ccc034c0bdf71c200683166edd1b7ae
MatchaTi <tehmatcha28@gmail.com>
2025-01-06 22:48:57 +0800

    fix: fixed bug close buffer

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -7,6 +7,34 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
-map('i', '<C-l>', function()
-  vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
-end, { desc = 'Copilot Accept', noremap = true, silent = true })
+map("i", "<C-l>", function()
+  vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
+end, { desc = "Copilot Accept", noremap = true, silent = true })
+
+-- Keyboard users
+vim.keymap.set("n", "<C-t>", function()
+  require("menu").open "default"
+end, {})
+
+-- mouse users + nvimtree users!
+vim.keymap.set({ "n", "v" }, "<RightMouse>", function()
+  require("menu.utils").delete_old_menus()
+
+  vim.cmd.exec '"normal! \\<RightMouse>"'
+
+  -- clicked buf
+  local buf = vim.api.nvim_win_get_buf(vim.fn.getmousepos().winid)
+  local options = vim.bo[buf].ft == "NvimTree" and "nvimtree" or "default"
+
+  require("menu").open(options, { mouse = true })
+end, {})
+
+vim.api.nvim_set_keymap("n", "<leader>x", ":lua CloseSidebarAndBuffer()<CR>", { noremap = true, silent = true })
+
+function CloseSidebarAndBuffer()
+  local sidebar_active = vim.fn.bufwinnr "nvim-tree"
+  if sidebar_active > 0 then
+    vim.cmd "NvimTreeClose"
+  end
+  vim.cmd "bd"
+end
