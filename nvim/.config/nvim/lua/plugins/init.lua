ff2433e6fde26a8bfa893d9d086ea4a16f9f7c14
MatchaTi <tehmatcha28@gmail.com>
2025-01-28 14:57:50 +0800

    chore: delete tailwind sorter cause deprecated😥

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -161,12 +161,12 @@ return {
     opts = {},
   },
 
-  {
-    'laytan/tailwind-sorter.nvim',
-    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-lua/plenary.nvim' },
-    build = 'cd formatter && npm ci && npm run build',
-    config = true,
-  },
+  -- { -- Deprecated
+  --   'laytan/tailwind-sorter.nvim',
+  --   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-lua/plenary.nvim' },
+  --   build = 'cd formatter && npm ci && npm run build',
+  --   config = true,
+  -- },
 
   {
     "adalessa/laravel.nvim",
