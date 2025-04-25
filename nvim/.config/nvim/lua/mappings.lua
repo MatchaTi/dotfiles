f25b86a1739a7b22fdbde5cca07d94bf351ee2e1
MatchaTi <tehmatcha28@gmail.com>
2025-04-26 00:24:58 +0800

    chore: edit keybind for copilot completions

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -7,7 +7,7 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
-map("i", "<C-l>", function()
+map("i", "<Tab>", function()
   vim.fn.feedkeys(vim.fn["copilot#Accept"](), "")
 end, { desc = "Copilot Accept", noremap = true, silent = true })
 
