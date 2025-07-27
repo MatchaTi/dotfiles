86d9dc76c59cff444d45cf2db5a80b6195923083
MatchaTi <tehmatcha28@gmail.com>
2025-07-27 14:45:16 +0800

    chore(lspsaga): float terminal

--- nvim/.config/nvim/lua/mappings.lua
+++ nvim/.config/nvim/lua/mappings.lua
@@ -41,3 +41,4 @@ vim.keymap.set('n', 'K', '<cmd>Lspsaga hover_doc<CR>', { silent = true, desc = '
 vim.keymap.set('n', 'J', '<cmd>Lspsaga peek_definition<CR>', { silent = true, desc = 'Peek Definition' })
 vim.keymap.set('n', '<C-f>', '<cmd>Lspsaga finder<CR>', { silent = true, desc = 'LSP Finder' })
 vim.keymap.set('n', '<leader>rr', '<cmd>Lspsaga rename<CR>', { silent = true, desc = 'LSP Rename' })
+vim.keymap.set({ 'n', 't' }, '<A-i>', '<cmd>Lspsaga term_toggle<CR>', { silent = true })
