e1fccb43e69a90f006eaab76e9b843687bc13b89
MatchaTi <tehmatcha28@gmail.com>
2025-08-17 20:07:06 +0800

    kdsknmnsv,mxnmvx

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -4,17 +4,3 @@ vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set nowrap")
 vim.opt.mouse = ""
-
-local augroup = vim.api.nvim_create_augroup('NvimTreeAutoClose', { clear = true })
-
-vim.api.nvim_create_autocmd('BufEnter', {
-    group = augroup,
-    nested = true,
-    callback = function()
-        if #vim.api.nvim_list_wins() == 1 and vim.bo.filetype == 'NvimTree' then
-            vim.schedule(function()
-                vim.cmd('quit')
-            end)
-        end
-    end,
-})
