4aedfc9762dd167bd77fd0ed49741c734a41aedd
MatchaTi <tehmatcha28@gmail.com>
2025-12-25 15:05:20 +0800

    chore: update dotfiles, add nvim noice plugin
    Update foot terminal font weight to semibold and adjust padding.
    Add noice.nvim for enhanced Neovim notifications and UI, along with its dependencies.
    Configure lualine with a transparent background and simplified separators.
    Adjust Sway inner and outer gaps, and update the default wallpaper.
    Include new wallpaper images.

--- nvim/.config/nvim/lua/configs/lualine.lua
+++ nvim/.config/nvim/lua/configs/lualine.lua
@@ -1,98 +1,99 @@
--- local theme = {
---     normal = {
---         a = { bg = "None" },
---         b = { bg = "None" },
---         c = { bg = "None" },
---         x = { bg = "None" },
---         y = { bg = "None" },
---         z = { bg = "None" },
---     },
---     insert = {
---         a = { bg = "None" },
---         b = { bg = "None" },
---         c = { bg = "None" },
---         x = { bg = "None" },
---         y = { bg = "None" },
---         z = { bg = "None" },
---     },
---     visual = {
---         a = { bg = "None" },
---         b = { bg = "None" },
---         c = { bg = "None" },
---         x = { bg = "None" },
---         y = { bg = "None" },
---         z = { bg = "None" },
---     },
---     replace = {
---         a = { bg = "None" },
---         b = { bg = "None" },
---         c = { bg = "None" },
---         x = { bg = "None" },
---         y = { bg = "None" },
---         z = { bg = "None" },
---     },
---     command = {
---         a = { bg = "None" },
---         b = { bg = "None" },
---         c = { bg = "None" },
---         x = { bg = "None" },
---         y = { bg = "None" },
---         z = { bg = "None" },
---     },
---     inactive = {
---         a = { bg = "None" },
---         b = { bg = "None" },
---         c = { bg = "None" },
---         x = { bg = "None" },
---         y = { bg = "None" },
---         z = { bg = "None" },
---     },
--- }
+local theme = {
+  normal = {
+    a = { bg = "None" },
+    b = { bg = "None" },
+    c = { bg = "None" },
+    x = { bg = "None" },
+    y = { bg = "None" },
+    z = { bg = "None" },
+  },
+  insert = {
+    a = { bg = "None" },
+    b = { bg = "None" },
+    c = { bg = "None" },
+    x = { bg = "None" },
+    y = { bg = "None" },
+    z = { bg = "None" },
+  },
+  visual = {
+    a = { bg = "None" },
+    b = { bg = "None" },
+    c = { bg = "None" },
+    x = { bg = "None" },
+    y = { bg = "None" },
+    z = { bg = "None" },
+  },
+  replace = {
+    a = { bg = "None" },
+    b = { bg = "None" },
+    c = { bg = "None" },
+    x = { bg = "None" },
+    y = { bg = "None" },
+    z = { bg = "None" },
+  },
+  command = {
+    a = { bg = "None" },
+    b = { bg = "None" },
+    c = { bg = "None" },
+    x = { bg = "None" },
+    y = { bg = "None" },
+    z = { bg = "None" },
+  },
+  inactive = {
+    a = { bg = "None" },
+    b = { bg = "None" },
+    c = { bg = "None" },
+    x = { bg = "None" },
+    y = { bg = "None" },
+    z = { bg = "None" },
+  },
+}
+
 local function vimIcon()
-    return [[]]
+  return [[]]
 end
 
 require('lualine').setup {
 
-    options = {
-        icons_enabled = true,
-        theme = theme,
-        -- component_separators = { left = '|', right = '|' },
-        -- section_separators = { left = '', right = '' },
-        component_separators = { left = '', right = '' },
-        section_separators = { left = '', right = '' },
-        -- component_separators = { left = '', right = '' },
-        -- section_separators = { left = '', right = '' },
-        globalstatus = true,
-    },
+  options = {
+    icons_enabled = true,
+    theme = theme,
+    -- component_separators = { left = '|', right = '|' },
+    -- section_separators = { left = '', right = '' },
+    -- component_separators = { left = '', right = '' },
+    -- section_separators = { left = '', right = '' },
+    component_separators = { left = '', right = '' },
+    section_separators = { left = '', right = '' },
+    globalstatus = true,
+  },
 
-    sections = {
-        lualine_a = { { vimIcon }, 'mode' },
-        lualine_b = { 'branch' },
-        lualine_c = { { 'filename', path = 1 },
-            {
-                'diff',
-                symbols = { added = ' ', modified = ' ', removed = ' ',
-                    untracked = ' ', renamed = ' ', stashed = ' '
-                },
-                colored = true
-            }
-            , 'diagnostics'
+  sections = {
+    lualine_a = { { vimIcon }, 'mode' },
+    lualine_b = { 'branch' },
+    lualine_c = { { 'filename', path = 1 },
+      {
+        'diff',
+        symbols = { added = ' ', modified = ' ', removed = ' ',
+          untracked = ' ', renamed = ' ', stashed = ' '
         },
-        lualine_x = { 'encoding', 'fileformat', 'filetype' },
-        lualine_y = { 'progress' },
-        lualine_z = { 'location' }
+        colored = true
+      }
+      , 'diagnostics'
     },
+    lualine_x = { 'encoding', 'fileformat', 'filetype' },
+    lualine_y = { 'progress' },
+    lualine_z = { 'location' }
+  },
 }
 
--- local modes = { 'normal', 'insert', 'visual', 'replace', 'command', 'terminal' }
--- local sections = { 'a', 'b', 'c', 'x', 'y', 'z' }
---
--- for _, mode in ipairs(modes) do
---     for _, section in ipairs(sections) do
---         vim.api.nvim_set_hl(0, 'lualine_' .. section .. '_' .. mode, { bg = 'NONE' })
---     end
--- end
---
--- vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'NONE' })
--- vim.api.nvim_set_hl(0, 'StatusLineNC', { bg = 'NONE' })
+local modes = { 'normal', 'insert', 'visual', 'replace', 'command', 'terminal' }
+local sections = { 'a', 'b', 'c', 'x', 'y', 'z' }
+
+for _, mode in ipairs(modes) do
+  for _, section in ipairs(sections) do
+    vim.api.nvim_set_hl(0, 'lualine_' .. section .. '_' .. mode, { bg = 'NONE' })
+  end
+end
+
+vim.api.nvim_set_hl(0, 'StatusLine', { bg = 'NONE' })
+vim.api.nvim_set_hl(0, 'StatusLineNC', { bg = 'NONE' })
--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -1,246 +1,261 @@
 return {
-    {
-        'nvim-telescope/telescope.nvim',
-        tag = '0.1.8',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        config = function()
-            require "configs.telescope"
-        end,
+  {
+    'nvim-telescope/telescope.nvim',
+    tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      require "configs.telescope"
+    end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    branch = 'master',
+    lazy = false,
+    build = ":TSUpdate",
+    config = function()
+      require "configs.treesitter"
+    end
+  },
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
+    config = function()
+      require "configs.file-browser"
+    end
+  },
+  {
+    "nvim-tree/nvim-web-devicons",
+    config = function()
+      require "configs.devicons"
+    end
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require "configs.lualine"
+    end
+  },
+  {
+    "mason-org/mason.nvim",
+    opts = {},
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    opts = {},
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
     },
-    {
-        "nvim-treesitter/nvim-treesitter",
-        branch = 'master',
-        lazy = false,
-        build = ":TSUpdate",
-        config = function()
-            require "configs.treesitter"
-        end
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls" }
+      })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end
+  },
+  {
+    'stevearc/conform.nvim',
+    opts = {},
+    config = function()
+      require "configs.conform"
+    end
+  },
+  {
+    "nvimtools/none-ls.nvim",
+    config = function()
+      require("null-ls").setup()
+    end,
+    requires = { "nvim-lua/plenary.nvim" },
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
     },
-    {
-        "nvim-telescope/telescope-file-browser.nvim",
-        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
-        config = function()
-            require "configs.file-browser"
-        end
+    dependencies = {
+      "nvim-lua/plenary.nvim",
     },
-    {
-        "nvim-tree/nvim-web-devicons",
-        config = function()
-            require "configs.devicons"
-        end
-    },
-    {
-        'nvim-lualine/lualine.nvim',
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
-        config = function()
-            require "configs.lualine"
-        end
-    },
-    {
-        "mason-org/mason.nvim",
-        opts = {},
-    },
-    {
-        "mason-org/mason-lspconfig.nvim",
-        opts = {},
-        dependencies = {
-            { "mason-org/mason.nvim", opts = {} },
-            "neovim/nvim-lspconfig",
-        },
-        config = function()
-            require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls" }
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            require "configs.lspconfig"
-        end
-    },
-    {
-        'stevearc/conform.nvim',
-        opts = {},
-        config = function()
-            require "configs.conform"
-        end
-    },
-    {
-        "nvimtools/none-ls.nvim",
-        config = function()
-            require("null-ls").setup()
-        end,
-        requires = { "nvim-lua/plenary.nvim" },
-    },
-    {
-        "kdheepak/lazygit.nvim",
-        lazy = true,
-        cmd = {
-            "LazyGit",
-            "LazyGitConfig",
-            "LazyGitCurrentFile",
-            "LazyGitFilter",
-            "LazyGitFilterCurrentFile",
-        },
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-        keys = {
-            { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
-        }
-    },
-    {
-        'lewis6991/gitsigns.nvim',
-        config = function()
-            require 'configs.gitsigns'
-        end
-    },
-    {
-        "hrsh7th/nvim-cmp",
-        config = function()
-            require "configs.cmp"
-        end
-    },
-    { "hrsh7th/cmp-buffer" },
-    { "hrsh7th/cmp-nvim-lsp" },
-    { "hrsh7th/cmp-path" },
-    { "saadparwaiz1/cmp_luasnip" },
-    {
-        'windwp/nvim-autopairs',
-        event = "InsertEnter",
-        config = true
-    },
-    {
-        "windwp/nvim-ts-autotag",
-        config = function()
-            require 'configs.autotag'
-        end
-    },
-    {
-        "rachartier/tiny-inline-diagnostic.nvim",
-        event = "VeryLazy",
-        priority = 1000,
-        config = function()
-            require 'configs.tiny-inline-diagnostic'
-            vim.diagnostic.config({ virtual_text = false })
-        end
-    },
-    {
-        "tiagovla/scope.nvim",
-        config = function()
-            require 'configs.scope'
-        end
-    },
-    {
-        "lukas-reineke/indent-blankline.nvim",
-        main = "ibl",
-        ---@module "ibl"
-        ---@type ibl.config
-        opts = {},
-        config = function()
-            require 'configs.indent-blankline'
-        end
-    },
-
-    {
-        'numToStr/Comment.nvim',
-        config = function()
-            require 'configs.comment'
-        end
-    },
-    {
-        'JoosepAlviste/nvim-ts-context-commentstring',
-        config = function()
-            require('ts_context_commentstring').setup {
-                enable_autocmd = false,
-            }
-        end
-    },
-    {
-        "onsails/lspkind.nvim",
-        config = function()
-            require 'configs.lspkind'
-        end
-    },
-    {
-        "github/copilot.vim",
-        lazy = false,
-        opts = {
-            suggestion = {
-                auto_trigger = true,
-                keymap = {
-                    accept = "<C-l>",
-                },
-            },
-        },
-        config = function()
-            require 'configs.copilot'
-        end,
-    },
-    {
-        "yioneko/nvim-vtsls",
-        config = function()
-            require 'configs.vtsls'
-        end,
-    },
-    {
-        "catgoose/nvim-colorizer.lua",
-        event = "BufReadPre",
-        config = function()
-            require 'configs.colorizer'
-        end
-    },
-    {
-        'projekt0n/github-nvim-theme',
-        name = 'github-theme',
-        lazy = false,    -- make sure we load this during startup if it is your main colorscheme
-        priority = 1000, -- make sure to load this before all the other start plugins
-        config = function()
-            require 'configs.github-theme'
-        end,
-    },
-    {
-        'nvimdev/lspsaga.nvim',
-        config = function()
-            require 'configs.lspsaga'
-        end,
-        dependencies = {
-            'nvim-treesitter/nvim-treesitter',
-            'nvim-tree/nvim-web-devicons',
-        }
-    },
-    {
-        "nvim-tree/nvim-tree.lua",
-        version = "*",
-        lazy = false,
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
-        config = function()
-            require "configs.nvim-tree"
-        end,
-    },
-    {
-        'MeanderingProgrammer/render-markdown.nvim',
-        dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
-        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
-        -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-        ---@module 'render-markdown'
-        ---@type render.md.UserConfig
-        opts = {},
-    },
-    {
-        "folke/todo-comments.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        opts = {}
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
     }
-    -- {
-    --     'akinsho/bufferline.nvim',
-    --     version = "*",
-    --     dependencies = 'nvim-tree/nvim-web-devicons',
-    --     config = function()
-    --         require 'configs.bufferline'
-    --     end
-    -- }
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require 'configs.gitsigns'
+    end
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    config = function()
+      require "configs.cmp"
+    end
+  },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-path" },
+  { "saadparwaiz1/cmp_luasnip" },
+  {
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    config = function()
+      require 'configs.autotag'
+    end
+  },
+  {
+    "rachartier/tiny-inline-diagnostic.nvim",
+    event = "VeryLazy",
+    priority = 1000,
+    config = function()
+      require 'configs.tiny-inline-diagnostic'
+      vim.diagnostic.config({ virtual_text = false })
+    end
+  },
+  {
+    "tiagovla/scope.nvim",
+    config = function()
+      require 'configs.scope'
+    end
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
+    config = function()
+      require 'configs.indent-blankline'
+    end
+  },
+
+  {
+    'numToStr/Comment.nvim',
+    config = function()
+      require 'configs.comment'
+    end
+  },
+  {
+    'JoosepAlviste/nvim-ts-context-commentstring',
+    config = function()
+      require('ts_context_commentstring').setup {
+        enable_autocmd = false,
+      }
+    end
+  },
+  {
+    "onsails/lspkind.nvim",
+    config = function()
+      require 'configs.lspkind'
+    end
+  },
+  {
+    "github/copilot.vim",
+    lazy = false,
+    opts = {
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = "<C-l>",
+        },
+      },
+    },
+    config = function()
+      require 'configs.copilot'
+    end,
+  },
+  {
+    "yioneko/nvim-vtsls",
+    config = function()
+      require 'configs.vtsls'
+    end,
+  },
+  {
+    "catgoose/nvim-colorizer.lua",
+    event = "BufReadPre",
+    config = function()
+      require 'configs.colorizer'
+    end
+  },
+  {
+    'projekt0n/github-nvim-theme',
+    name = 'github-theme',
+    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require 'configs.github-theme'
+    end,
+  },
+  {
+    'nvimdev/lspsaga.nvim',
+    config = function()
+      require 'configs.lspsaga'
+    end,
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter',
+      'nvim-tree/nvim-web-devicons',
+    }
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require "configs.nvim-tree"
+    end,
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {}
+  },
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    }
+  }
+  -- {
+  --     'akinsho/bufferline.nvim',
+  --     version = "*",
+  --     dependencies = 'nvim-tree/nvim-web-devicons',
+  --     config = function()
+  --         require 'configs.bufferline'
+  --     end
+  -- }
 }
