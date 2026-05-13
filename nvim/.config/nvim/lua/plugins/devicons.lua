6437d7f558106594c6cc37ad6a6147cfe5f5dede
MatchaTi <tehmatcha28@gmail.com>
2026-05-13 11:50:58 +0800

    nvim

---
+++ nvim/.config/nvim/lua/plugins/devicons.lua
@@ -0,0 +1,12 @@
+require("nvim-web-devicons").set_icon {
+    ts = {
+        icon = "",
+        color = "#3178c6",
+        name = "TsIcon"
+    },
+    js = {
+        icon = "",
+        color = "#f1e05a",
+        name = "JsIcon"
+    },
+}
